c752b0a8e6a5a184ac5dca0878a6bb955ab0654c
Boyuzhou <386607913@qq.com>
2016-12-05 09:00:49 +0800

    fix bug

--- build/Overlay.js
+++ build/Overlay.js
@@ -109,10 +109,11 @@ var Overlay = function (_Component) {
   }
 
   Overlay.prototype.render = function render() {
-    var _props = this.props,
-        animation = _props.animation,
-        children = _props.children,
-        props = _objectWithoutProperties(_props, ['animation', 'children']);
+    var _props = this.props;
+    var animation = _props.animation;
+    var children = _props.children;
+
+    var props = _objectWithoutProperties(_props, ['animation', 'children']);
 
     var transition = animation === true ? _Fade2["default"] : animation || null;
 
