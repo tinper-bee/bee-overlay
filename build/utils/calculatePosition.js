af596b47167f6ad60590a89eab0c010d893998a8
izbz wh <731215820@qq.com>
2021-01-15 10:50:06 +0800

    fix: 发布

--- build/utils/calculatePosition.js
+++ build/utils/calculatePosition.js
@@ -55,8 +55,8 @@ function getTopDelta(top, overlayHeight, container, padding) {
 
     if (topEdgeOffset < 0) {
         return -topEdgeOffset;
-    } else if (bottomEdgeOffset > containerHeight) {
-        return containerHeight - bottomEdgeOffset;
+    } else if (bottomEdgeOffset > containerHeight - containerScroll) {
+        return containerHeight - bottomEdgeOffset - containerScroll;
     } else {
         return 0;
     }
